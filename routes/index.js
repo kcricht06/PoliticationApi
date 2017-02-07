d7de1ac50932b3987a1a100e59b349d57c9e093b
Kyle Crichton <kylecrichton@gmail.com>
2017-02-07 13:09:28 -0600

    added first api call

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+var request = require('request');
+
+var headers = {
+    'Content-Type': 'application/json'
+};
+
+var dataString = '{"bio_ids": ["C000880", "A000360"]}';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res) {
+  var options = {
+    url: 'https://congressforms.eff.org/retrieve-form-elements/',
+    method: 'POST',
+    headers: headers,
+    body: dataString
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(body);
+    }
+}
+
+request(options, callback);
+
+});
+
+module.exports = router;
